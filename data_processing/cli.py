4ef3f1941c32fcf588fb850633e2bf2c37f6a878
Miguel González Duque <miguelgondu@gmail.com>
2022-10-19 15:24:20 +0200

    Updates the CLI a little bit by adding max number of words

--- data_processing/cli.py
+++ data_processing/cli.py
@@ -14,20 +14,20 @@ def cli():
 @click.argument("subreddits", nargs=-1)
 @click.option("--feed", default="hot")
 @click.option("--limit", type=int, default=100)
-def reddit(output, subreddits, feed, limit):
+@click.option("--max-num-of-words", type=int, default=70)
+def reddit(output, subreddits, feed, limit, max_num_of_words):
     reddits = []
     for subr in subreddits:
         reddits += get_reddits(subr, feed, limit)
     df = reddits_to_df(reddits)
 
-    new_df = pd.DataFrame(columns=["joke", "label"])
-
-    max_num_of_words = 70
-
+    # Creatign the new table with the jokes themselves
+    new_df = pd.DataFrame(columns=["joke", "label", "joke_length_in_words"])
     new_df["joke"] = df["text"] + " " + df["content"]
     new_df["label"] = subreddits[0]
-    new_df["joke_length_in_words"] = new_df["joke"].apply(lambda joke: len(joke.split(" ")))
-
+    new_df["joke_length_in_words"] = new_df["joke"].apply(
+        lambda joke: len(joke.split(" "))
+    )
     new_df = new_df[new_df["joke_length_in_words"] <= max_num_of_words]
 
     new_df.to_csv(output)
