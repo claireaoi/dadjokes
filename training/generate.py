3855be9531e514a5b50be0ea67177fa0128469c9
Claire Brigitte Glanois <claire.glanois@gmail.com>
2022-10-19 15:48:20 +0200

    Train Generate

--- training/generate.py
+++ training/generate.py
@@ -1,10 +1,10 @@
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 from transformers import AutoTokenizer
 
-MODELPATH="./models/v1/"
+MODELPATH="distilbert-base-uncased"#"./models/v1/"
+
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 model = AutoModelForSequenceClassification.from_pretrained(MODELPATH)#pad_token_id=tokenizer.eos_token_id)
-
 
 
 ################################################
@@ -15,9 +15,22 @@ model = AutoModelForSequenceClassification.from_pretrained(MODELPATH)#pad_token_
 joke="I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along."
 print("#####TESTING joke {}######".format(joke))
 
+#NEED tokenize?
+#jokeTKN = tokenizer.encode(joke, return_tensors = "pt")
+#NEED PIPELINE ?
+#classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
 
-#tokenize
-jokeTKN = tokenizer.encode(joke, return_tensors = "pt")
-#generate new line
+outputTKN = model(joke)#max_length?
 
-outputTKN = model.generate(jokeTKN, do_sample=True, temperature=0.1,)#max_length?
+#WHAT IS THE OUTPUT FORMAT ?
+# Output:
+# [[
+# {'label': 'sadness', 'score': 0.0006792712374590337}, 
+# {'label': 'joy', 'score': 0.9959300756454468}, 
+# {'label': 'love', 'score': 0.0009452480007894337}, 
+# {'label': 'anger', 'score': 0.0018055217806249857}, 
+# {'label': 'fear', 'score': 0.00041110432357527316}, 
+# {'label': 'surprise', 'score': 0.0002288572577526793}
+# ]]
+
+print(outputTKN)
