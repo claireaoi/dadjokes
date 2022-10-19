ed966b88fd5082ccb123c9ae7ad1748fb7e1fcae
Claire Brigitte Glanois <claire.glanois@gmail.com>
2022-10-19 20:22:29 +0200

    Generate 1

--- training/generate.py
+++ training/generate.py
@@ -8,30 +8,13 @@ MODELPATH="./models/v1/"
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 model = AutoModelForSequenceClassification.from_pretrained(MODELPATH)#pad_token_id=tokenizer.eos_token_id)
 
-################################################
-######## GENERATE
-################################################
 joke="I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along."
-
-print("#####TESTING joke {}######".format(joke))
-# print("*******TEST 1******")
-# jokeTKN = tokenizer.encode(joke, return_tensors = "pt")
-# output = model(jokeTKN)['logits']
-# print(output)
-# amax=torch.amax(output, 1).item()
-# print(amax)
-
-# print("*******TEST 2******")
-
 
 def evaluate(joke):
     classifier = pipeline("text-classification", model=MODELPATH, top_k=1)#return_all_scores=True)
 
     #NOTE: to return all score, use top_k=None
-    out_raw=classifier(joke)[0]
-    print(out_raw)
-    out=out_raw[0]['label']
-    print(out)
+    out=classifier(joke)[0][0]['label']
     if out=='LABEL_1':
         return "not-dadjokes"
     else:
