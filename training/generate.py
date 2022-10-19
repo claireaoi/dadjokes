30d9fe786da8b288edcdc38aee982047a9d7e90a
Claire Brigitte Glanois <claire.glanois@gmail.com>
2022-10-19 20:11:10 +0200

    Generate 1

--- training/generate.py
+++ training/generate.py
@@ -14,14 +14,21 @@ model = AutoModelForSequenceClassification.from_pretrained(MODELPATH)#pad_token_
 joke="I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along."
 
 print("#####TESTING joke {}######".format(joke))
-print("*******TEST 1******")
-jokeTKN = tokenizer.encode(joke, return_tensors = "pt")
-output = model(jokeTKN)['logits']
-amax=torch.amax(output, 1).item()
-print(amax)
+# print("*******TEST 1******")
+# jokeTKN = tokenizer.encode(joke, return_tensors = "pt")
+# output = model(jokeTKN)['logits']
+# print(output)
+# amax=torch.amax(output, 1).item()
+# print(amax)
+
+# print("*******TEST 2******")
 
 
-print("*******TEST 2******")
-classifier = pipeline("text-classification",model=MODELPATH, return_all_scores=True)
-print("output", classifier(joke))
+def generate(joke):
+    classifier = pipeline("text-classification",model=MODELPATH, top_k=1)#return_all_scores=True)
+    #NOTE: to return all score, use top_k=None
+    out=classifier(joke)
+    print("output", classifier(joke))
+    return out
 
+generate(joke)
